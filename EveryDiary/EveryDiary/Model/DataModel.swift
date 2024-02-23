4d9eeba2ea52914b179660d5b9a489d8a20c07ab
ulls12 <koes823@gmail.com>
2024-02-23 15:56:30 +0900

    3차 초기세팅

---
+++ EveryDiary/EveryDiary/Model/DataModel.swift
@@ -0,0 +1,8 @@
+//
+//  DataModel.swift
+//  EveryDiary
+//
+//  Created by t2023-m0044 on 2/23/24.
+//
+
+import Foundation
---
+++ EveryDiary/EveryDiary/RemoteDB/FireStore.swift
@@ -0,0 +1,9 @@
+//
+//  FireStore.swift
+//  EveryDiary
+//
+//  Created by t2023-m0044 on 2/23/24.
+//
+
+import Foundation
+import Firebase
