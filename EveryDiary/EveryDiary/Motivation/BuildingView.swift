ec50d0285fc43d1276715f2bbb80fd82e180730a
EojinEva <w-w-w-@naver.com>
2024-03-04 21:12:00 +0900

    [Chore] : 삼중 for문 수정

--- EveryDiary/EveryDiary/Motivation/BuildingView.swift
+++ EveryDiary/EveryDiary/Motivation/BuildingView.swift
@@ -49,7 +49,6 @@ class BuildingView: UIView {
         drawBackBuilding()
         drawBuilding()
         drawWindowInBuilding()
-        //layoutSubviews()
     }
     
     // UIView에 맞춰 동적으로 크기 변경
@@ -180,6 +179,106 @@ class BuildingView: UIView {
         buildingLayer.addSublayer(windowLayer)
     }
     
+    func drawWindowInBuilding() {
+        print("drawWindowInBuilding() called")
+        var windowOrder = 1
+        
+        let diaryCount = diaryDays.count
+        
+        if diaryCount <= 7 {
+            handleBuilding(buildings[0], &windowOrder)
+        } else if diaryCount <= 14 {
+            handleBuilding(buildings[0], &windowOrder)
+            handleBuilding(buildings[1], &windowOrder)
+        } else if diaryCount <= 21 {
+            handleBuilding(buildings[0], &windowOrder)
+            handleBuilding(buildings[1], &windowOrder)
+            handleBuilding(buildings[2], &windowOrder)
+        } else if diaryCount <= 28 {
+            handleBuilding(buildings[0], &windowOrder)
+            handleBuilding(buildings[1], &windowOrder)
+            handleBuilding(buildings[2], &windowOrder)
+            handleBuilding(buildings[3], &windowOrder)
+        } else if diaryCount <= 30 {
+            handleBuilding(buildings[0], &windowOrder)
+            handleBuilding(buildings[1], &windowOrder)
+            handleBuilding(buildings[2], &windowOrder)
+            handleBuilding(buildings[3], &windowOrder)
+            handleBuilding(buildings[4], &windowOrder)
+        } else if diaryCount >= 31 {
+            handleBuilding(buildings[0], &windowOrder)
+            handleBuilding(buildings[1], &windowOrder)
+            handleBuilding(buildings[2], &windowOrder)
+            handleBuilding(buildings[3], &windowOrder)
+            handleBuilding(buildings[4], &windowOrder)
+            handleBuilding(buildings[5], &windowOrder)
+        }
+    }
+    
+    func handleBuilding(_ building: BuildingSize, _ windowOrder: inout Int) {
+        //i는 현재 층의 인덱스, row는 현재 층의 창문 배열. 각 층에 대해 창문을 그리기 위해 층의 각 창문을 순회.
+        for (i, row) in building.windowLayout.columns.enumerated() {
+            //각 층의 창문을 순회. j는 현재 창문의 인덱스, columns는 현재 창문의 수
+            for (j, columns) in row.enumerated() {
+                //창문 행렬에서 0은 데이터 비교에서 제외.
+                if columns == 0 { continue }
+                //각 건물의 창문을 그릴 위치를 계산
+                //각 창문의 너비 계산. 모든 창문의 너비는 동일하므로 계산을 통해 각 창문의 x 좌표를 결정
+                let windowWidth = building.size.width / CGFloat(columns)
+                //창문을 그리기 위해서 각 건물의 층의 높이를 계산. 건물의 전체 높이를 층 수로 나눔 = 각 층의 높이. 각 창문의 y 좌표를 결정
+                let windowHeight = building.size.height / CGFloat(building.windowLayout.columns.count)
+                //각 창문의 위치를 계산
+                let windowPosition = CGPoint(x: building.position.x + windowWidth * CGFloat(j), y: building.position.y - windowHeight * CGFloat(i+1))
+                //diaryDays 배열에 창문의 순서가 포함되어 있는지 확인하고 값이 있으면 노란색
+                if diaryDays.contains(windowOrder) {
+                    self.drawWindows(at: windowPosition, color: .yellow)
+                    print("Window \(windowOrder): 데이터 있음")
+                } else {
+                    self.drawWindows(at: windowPosition, color: .darkGray)
+                    print("Window \(windowOrder): 데이터 없음")
+                }
+                //창문의 순서를 증가시키기. 다음 창문에 대해 새로운 번호 부여. 건물의 층이나 창문 배치에 따라 windowOrder의 값은 1, 2, 3 등으로 순차적으로 증가하지 않을 수 있음.
+                windowOrder += 1
+            }
+        }
+    }
+}
+
+
+//MARK: firebase
+extension BuildingView {
+    func fetchDiariesForCurrentMonth(year: Int, month: Int, completion: @escaping ([DiaryEntry]?, Error?) -> Void) {
+        func getUserID() -> String? {
+            return Auth.auth().currentUser?.uid
+        }
+        
+        guard let userID = getUserID() else {
+            completion(nil, NSError(domain: "Auth Error", code: 401, userInfo: nil))
+            return
+        }
+        
+        let startOfMonth = "\(year)-\(String(format: "%02d", month))-01 00:00:00 +0000"
+        let endOfMonth = month == 12 ? "\(year + 1)-01-01 23:59:59 +0000" : "\(year)-\(String(format: "%02d", month + 1))-01 23:59:59 +0000"
+        
+        db.collection("users").document(userID).collection("diaries").whereField("dateString", isGreaterThanOrEqualTo: startOfMonth).whereField("dateString", isLessThan: endOfMonth).getDocuments { (querySnapshot, error) in
+            if let error = error {
+                print("Error getting documents: \(error)")
+                completion(nil, error)
+            } else {
+                var diaries = [DiaryEntry]()
+                for document in querySnapshot!.documents {
+                    if let diary = try? document.data(as: DiaryEntry.self) {
+                        diaries.append(diary)
+                    }
+                }
+                DispatchQueue.main.async { // 메인 스레드에서 로그를 출력합니다.
+                    print("Fetched diaries: \(diaries)")
+                }
+                completion(diaries, nil)
+            }
+        }
+    }
+    
     func windowsInBuildingData() {
         let currentYear = Calendar.current.component(.year, from: Date())
         let currentMonth = Calendar.current.component(.month, from: Date())
@@ -213,69 +312,6 @@ class BuildingView: UIView {
                     print("Position: \(building.position), Size: \(building.size)")
                     print("Window Layout: \(building.windowLayout.columns)")
                 }
-            }
-        }
-    }
-    
-    func drawWindowInBuilding() {
-        print("drawWindowInBuilding() calledSSIBAL WHY")
-        
-        var windowOrder = 1
-        
-        for (buildingIndex, building) in self.buildings.enumerated() {
-            let windowHeight = building.size.height / CGFloat(building.windowLayout.columns.count)
-            for (i, row) in building.windowLayout.columns.enumerated() {
-                for (j, columns) in row.enumerated() {
-                    //0은 데이터 비교 안 함
-                    if columns == 0 { continue }
-                    //각 건물의 창문을 그릴 위치를 계산
-                    let windowWidth = building.size.width / CGFloat(columns)
-                    let windowPosition = CGPoint(x: building.position.x + windowWidth * CGFloat(j), y: building.position.y - windowHeight * CGFloat(i+1))
-                    
-                    if diaryDays.contains(windowOrder) {
-                        self.drawWindows(at: windowPosition, color: .yellow)
-                        print("Building \(buildingIndex + 1), Window \(windowOrder): 데이터 있음")
-                    } else {
-                        self.drawWindows(at: windowPosition, color: .lightGray)
-                        print("Building \(buildingIndex + 1), Window \(windowOrder): 데이터 없음")
-                    }
-                    windowOrder += 1
-                }
-            }
-        }
-    }
-}
-
-extension BuildingView {
-    func fetchDiariesForCurrentMonth(year: Int, month: Int, completion: @escaping ([DiaryEntry]?, Error?) -> Void) {
-        func getUserID() -> String? {
-            return Auth.auth().currentUser?.uid
-        }
-        
-        guard let userID = getUserID() else {
-            completion(nil, NSError(domain: "Auth Error", code: 401, userInfo: nil))
-            return
-        }
-        
-        let startOfMonth = "\(year)-\(String(format: "%02d", month))-01 00:00:00 +0000"
-        let endOfMonth = month == 12 ? "\(year + 1)-01-01 23:59:59 +0000" : "\(year)-\(String(format: "%02d", month + 1))-01 23:59:59 +0000"
-        
-        
-        db.collection("users").document(userID).collection("diaries").whereField("dateString", isGreaterThanOrEqualTo: startOfMonth).whereField("dateString", isLessThan: endOfMonth).getDocuments { (querySnapshot, error) in
-            if let error = error {
-                print("Error getting documents: \(error)")
-                completion(nil, error)
-            } else {
-                var diaries = [DiaryEntry]()
-                for document in querySnapshot!.documents {
-                    if let diary = try? document.data(as: DiaryEntry.self) {
-                        diaries.append(diary)
-                    }
-                }
-                DispatchQueue.main.async { // 메인 스레드에서 로그를 출력합니다.
-                    print("Fetched diaries: \(diaries)")
-                }
-                completion(diaries, nil)
             }
         }
     }
