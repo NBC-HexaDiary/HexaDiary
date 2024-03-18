27a08abf78c6c3547377d4d0b83abb88150e5be0
ulls12 <koes823@gmail.com>
2024-03-18 12:12:15 +0900

    [Fix] Fix File Crash Issue

--- EveryDiary/EveryDiary/PushNotification/NotificationCell.swift
+++ EveryDiary/EveryDiary/PushNotification/NotificationCell.swift
@@ -29,7 +29,7 @@ class NotificationCell: UITableViewCell {
     private lazy var alarmSwtich : UISwitch = {
         let alarmSwitch = UISwitch()
         alarmSwitch.onTintColor = .mainTheme
-        alarmSwitch.addTarget(self, action: #selector(switchValueDidChange), for: .valueChanged)
+        alarmSwitch.addTarget(self, action: #selector(switchChanged), for: .valueChanged)
         return alarmSwitch
     }()
     
@@ -71,8 +71,50 @@ class NotificationCell: UITableViewCell {
         }
     }
     
-    @objc func switchValueDidChange(_ sender: UISwitch) {
-        switchValueChanged?(sender.isOn)
+    @objc func switchChanged(_ sender: UISwitch) {
+        // 스위치 상태 변경을 바로 적용하지 않고, 권한 요청 과정을 거칩니다.
+        // 권한 상태를 확인하고, 필요한 경우 권한을 요청합니다.
+        let center = UNUserNotificationCenter.current()
+        center.getNotificationSettings { settings in
+            DispatchQueue.main.async {
+                if settings.authorizationStatus == .authorized {
+                    // 권한이 이미 승인된 경우, 스위치 상태를 변경합니다.
+                    self.switchValueChanged?(sender.isOn)
+                } else if settings.authorizationStatus == .denied {
+                    // 권한이 거부된 경우, 설정으로 유도합니다.
+                    self.redirectToSettings()
+                    // 스위치 상태를 원래대로 되돌립니다.
+                    sender.setOn(false, animated: true)
+                } else {
+                    // 권한이 아직 요청되지 않은 경우, 권한을 요청합니다.
+                    self.requestNotificationPermission { granted in
+                        if granted {
+                            // 권한 요청이 승인된 경우
+                            self.switchValueChanged?(sender.isOn)
+                        } else {
+                            // 권한 요청이 거부된 경우
+                            sender.setOn(false, animated: true)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    // NotificationVC 또는 NotificationCell 내부
+    private func requestNotificationPermission(completion: @escaping (Bool) -> Void) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { granted, _ in
+            DispatchQueue.main.async {
+                completion(granted)
+            }
+        }
+    }
+
+    private func redirectToSettings() {
+        guard let settingsUrl = URL(string: UIApplication.openSettingsURLString), UIApplication.shared.canOpenURL(settingsUrl) else {
+            return
+        }
+        UIApplication.shared.open(settingsUrl, options: [:], completionHandler: nil)
     }
     
     func prepare(title: String, iconImage: String, switchStatus: Bool) {
@@ -81,5 +123,3 @@ class NotificationCell: UITableViewCell {
         self.alarmSwtich.isOn = switchStatus
     }
 }
-
-
