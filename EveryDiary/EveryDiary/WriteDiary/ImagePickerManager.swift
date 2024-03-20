c4f89904cf7d1e81a759f416931fdd93222b5557
gyeongeun <club2548@gmail.com>
2024-03-20 20:38:59 +0900

    [Feat] 사진 선택 완료 시, 위치 선택 기능 구현 사진 선택 완료 시, 사용자의 위치와 현재시간 or 촬영한 사진의 위치와 시간 중에 선택할 수 있도록 구현

--- EveryDiary/EveryDiary/WriteDiary/ImagePickerManager.swift
+++ EveryDiary/EveryDiary/WriteDiary/ImagePickerManager.swift
@@ -12,10 +12,12 @@ import UIKit
 // 이미지 선택 완료 시 처리를 위한 프로토콜 정의
 protocol ImagePickerDelegate: AnyObject {
     func didPickImages(_ imagesLocationInfo: [ImageLocationInfo], retainedIdentifiers: [String])
+    func timeAndLocationChoiceAlert(time: String, address: String, completion: @escaping (Bool) -> Void)
 }
 
 class ImagePickerManager: NSObject, PHPickerViewControllerDelegate {
     weak var delegate: ImagePickerDelegate?
+    weak var presentingViewController: UIViewController?
     
     // 이미 선택했던 사진들을 구분하는 assetIdentifier를 저장하기 위한 변수 선언. showsDiary에서 업데이트
     var selectedPhotoIdentifiers: [String] = []
@@ -23,6 +25,7 @@ class ImagePickerManager: NSObject, PHPickerViewControllerDelegate {
     // PHPicker를 호출하는 메서드
     func presentImagePicker(from viewController: UIViewController, selectedPhotoIdentifiers: [String]) {
         self.selectedPhotoIdentifiers = selectedPhotoIdentifiers
+        self.presentingViewController = viewController
         
         // PHPicker의 설정
         var configuration = PHPickerConfiguration(photoLibrary: .shared())
@@ -67,7 +70,12 @@ class ImagePickerManager: NSObject, PHPickerViewControllerDelegate {
                     if let asset = assets.firstObject {
                         let captureTime = asset.creationDate?.description
                         let location = asset.location
-                        let locationInfo = location.map {LocationInfo(latitude: $0.coordinate.latitude, longitude: $0.coordinate.longitude)}
+                        var locationInfo: LocationInfo?
+                        if let location = location {
+                            locationInfo = LocationInfo(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+                        } else {
+                            locationInfo = nil
+                        }
                         let locationString = location.map { "\($0.coordinate.latitude), \($0.coordinate.longitude)" }
                         
                         // 위치 정보가 있을 경우에만 locationInfo생성
@@ -78,7 +86,7 @@ class ImagePickerManager: NSObject, PHPickerViewControllerDelegate {
                             captureTime: captureTime,
                             location: locationString
                         )
-                        
+                        print("imageLocationInfo: \(imageLocationInfo)")
                         DispatchQueue.main.async {
                             newImagesLocationInfo.append(imageLocationInfo)
                         }
@@ -90,11 +98,31 @@ class ImagePickerManager: NSObject, PHPickerViewControllerDelegate {
         }
         // 모든 선택 작업이 완료되면, delegate으로 결과 전달
         group.notify(queue: .main) { [weak self] in
-            guard let self = self else { return }
+            print("group.notify")
+            guard let self = self, let firstImageLocationInfo = newImagesLocationInfo.first else { return }
             // identifiers 저장
             self.selectedPhotoIdentifiers = newResultsIdentifiers
-            // 새로운 선택결과를 delegate 메서드로 전달
-            self.delegate?.didPickImages(newImagesLocationInfo, retainedIdentifiers: newResultsIdentifiers)
+            // 위치 정보 유무에 따라 다음 단계 처리
+            if let firstImageLocationInfo = newImagesLocationInfo.first, let location = firstImageLocationInfo.locationInfo {
+                // 위치 정보가 있다면
+                let mapManger = MapManager()
+                mapManger.getPlaceName(latitude: location.latitude, longitude: location.longitude) { address in
+                    let time = firstImageLocationInfo.captureTime ?? self.formattedDateString(for: Date())
+                    // 위치 정보가 있는 경우에만 timeAndLocationChoiceAlert 호출
+                    // 선택 사진의 시간과 위치 정보를 사용할 것인지 확인하는 메서드
+                    self.delegate?.timeAndLocationChoiceAlert(time: time, address: address) { useMetadata in }
+                    // 새로운 선택결과를 delegate 메서드로 전달
+                    self.delegate?.didPickImages(newImagesLocationInfo, retainedIdentifiers: newResultsIdentifiers)
+                }
+            } else {
+                // 위치 정보가 없는 경우 didPickImages만 호출
+                self.delegate?.didPickImages(newImagesLocationInfo, retainedIdentifiers: newResultsIdentifiers)
+                if let presentingViewController = self.presentingViewController {
+                    let alert = UIAlertController(title: "사진의 위치정보가 없습니다. 사용자의 현재 위치로 대체됩니다.", message: nil, preferredStyle: .actionSheet)
+                    presentingViewController.present(alert, animated: true, completion: nil)
+                    Timer.scheduledTimer(withTimeInterval: 2.0, repeats: false, block: { _ in alert.dismiss(animated: true, completion: nil)})
+                }
+            }
         }
     }
 }
@@ -120,12 +148,16 @@ extension ImagePickerManager {
     
     // 접근 권한이 없을 경우 설정 화면으로 유도하는 AlertController
     private func showSettingsAlert(from viewController: UIViewController) {
-        let alert = UIAlertController(title: "사진을 첨부할 수 없습니다.", message: "사진을 추가하기 위해 사진 접근 권한을 수정해주세요.", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "취소", style: .cancel))
-        alert.addAction(UIAlertAction(title: "설정", style: .default) { _ in
+        let requestPhotoServiceAlert = UIAlertController(title: "사진을 첨부할 수 없습니다.", message: "사진을 추가하기 위해 사진 접근 권한을 수정해주세요.", preferredStyle: .alert)
+        requestPhotoServiceAlert.addAction(UIAlertAction(title: "취소", style: .cancel))
+        requestPhotoServiceAlert.addAction(UIAlertAction(title: "설정", style: .default) { _ in
             guard let settingsURL = URL(string: UIApplication.openSettingsURLString) else { return }
             UIApplication.shared.open(settingsURL)
         })
-        viewController.present(alert, animated: true)
+        viewController.present(requestPhotoServiceAlert, animated: true)
+    }
+    // Firestore 날짜저장 형식
+    func formattedDateString(for date: Date) -> String {
+        return DateFormatter.yyyyMMddHHmmss.string(from: date)
     }
 }
