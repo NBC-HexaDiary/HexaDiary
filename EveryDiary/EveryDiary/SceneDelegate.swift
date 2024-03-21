16185a2e38eef95bb2250865e845819cbc365906
동영 <arhaxoda@naver.com>
2024-03-21 16:14:06 +0900

    [Feat] #204 화면가리기

--- EveryDiary/EveryDiary/SceneDelegate.swift
+++ EveryDiary/EveryDiary/SceneDelegate.swift
@@ -33,34 +33,26 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        
     }
     
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        removeBlurEffect()
     }
     
     func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
+        addBlurEffect()
     }
     
     func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
         let biometricsEnabled = UserDefaults.standard.bool(forKey: "BiometricsEnabled")
         
         if biometricsEnabled {
-            self.addBlurEffect()
             
-            BiometricsAuth().authenticateWithBiometrics { [weak self] success in
+            BiometricsAuth().authenticateWithBiometrics { success in
                 DispatchQueue.main.async {
                     if success {
-                        self?.removeBlurEffect()
+                        print("성공")
                     } else {
                         print("login 실패")
                     }
@@ -70,9 +62,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
+        
     }
 }
 
@@ -81,7 +71,7 @@ extension SceneDelegate {
     private func addBlurEffect() {
         guard let window = window else { return }
         
-        let blurEffect = UIBlurEffect(style: .dark)
+        let blurEffect = UIBlurEffect(style: .light)
         blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView?.frame = window.bounds
         blurEffectView?.autoresizingMask = [.flexibleWidth, .flexibleHeight]
